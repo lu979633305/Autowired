dc8fe1ebe6adc1ad30c9a33aacfc8309a430b9fc
jianhua_glb <jianhua_glb@email.com>
2018-11-20 16:58:26 +0800

    AutowiredUser类

---
+++ src/main/java/com/qixin/AutowiredUser.java
@@ -0,0 +1,14 @@
+package com.qixin;
+
+public class AutowiredUser {
+   private String userName;
+   private int age;
+   private String sex;
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+   
+}
